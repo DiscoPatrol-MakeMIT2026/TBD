d124f554c076f8dd24a53166f3cec7f5334563a5
EliasCoumine <e.coumine@gmail.com>
2026-02-22 11:05:46 -0500

    Integrated both LEDs with YOLO detection

--- LED/led.py
+++ LED/led.py
@@ -1,16 +1,23 @@
+
 import board
 import neopixel
 import time
 import colorsys
 
 NUM_LEDS = 300
-PIN = board.D18
+PIN = board.D25
 
 pixels = neopixel.NeoPixel(PIN, NUM_LEDS, brightness=0.5, auto_write=False)
 
 def alarm_alert():
-    pixels.fill((255, 0, 0))
-    pixels.show()
+     for _ in range(5):  # number of flashes
+        pixels.fill((255, 0, 0))  # RED ON
+        pixels.show()
+        time.sleep(0.2)
+
+        pixels.fill((0, 0, 0))  # OFF
+        pixels.show()
+        time.sleep(0.2)
 
 def disco_cycle(wait=0.01):
     for j in range(256):
